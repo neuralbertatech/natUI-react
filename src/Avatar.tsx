a1ce1b3aec6da11c55a31bdf69888b8654afa651
Giancarlo Pernudi Segura <pernudig@gmail.com>
2022-10-08 21:52:45 -0400

    Avatar: The Way of Water

---
+++ src/Avatar.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from "react";
+import BaseProps from "./baseProps";
+import * as RadixAvatar from "@radix-ui/react-avatar";
+
+export interface AvatarProps extends BaseProps {
+  src: string;
+  alt: string;
+}
+
+const Avatar: FunctionComponent<AvatarProps> = (props) => {
+  const initial = props.alt.length > 0 ? props.alt[0].toUpperCase() : "";
+  return (
+    <RadixAvatar.Root>
+      <RadixAvatar.Image src={props.src} alt={props.alt} />
+      <RadixAvatar.Fallback>{initial}</RadixAvatar.Fallback>
+    </RadixAvatar.Root>
+  );
+};
+
+export default Avatar;
