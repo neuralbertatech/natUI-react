7a3e7d0c93adce64a385304d702686b65e271ddc
Giancarlo Pernudi Segura <pernudig@gmail.com>
2022-10-18 05:18:11 +0000

    BaseProps jsdoc

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
 import { CSSProperties } from "react";
 
+/**
+ * BaseProps
+ * @interface
+ * @property [style] {CSSProperties} Override CSS properties
+ * @see https://github.com/frenic/csstype 
+ */
 interface BaseProps {
 	style?: CSSProperties;
 }
