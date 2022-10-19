23d385783f5c14e0f9f55f74f750df00e694ae66
Giancarlo Pernudi Segura <gino@neuralberta.tech>
2022-10-19 04:33:26 +0000

    notification list component jsdoc

--- src/components/notification/NotificationList.tsx
+++ src/components/notification/NotificationList.tsx
@@ -1,16 +1,32 @@
+/**
+ * @module @neuralbertatech/ui-react
+ */
 import BaseProps from "../../types";
 import { FunctionComponent } from "react";
 import List from "../List";
 import Notification from "./Notification";
 import type { NotificationProps } from "./Notification";
 
+/**
+ * @interface
+ * @implements BaseProps
+ * @property notifications {NotificationProps[]} The notifications to display.
+ * @property [maxNotifications=5] {numbe} Maximum amount of notification to display at once. Will display the first n notifications.
+ * @property [maxTime=5000] {number} The maximum amount of time a notification should be on screen for.
+ */
 interface NotificationListProps extends BaseProps {
   notifications: NotificationProps[];
   maxNotifications?: number;
-  maxTime?: number; // in milliseconds
+  maxTime?: number;
   reversed?: boolean;
 }
 
+/**
+ * @component
+ * @param notifications {NotificationProps[]} The notifications to display.
+ * @param [maxNotifications=5] {numbe} Maximum amount of notification to display at once. Will display the first n notifications.
+ * @param [maxTime=5000] {number} The maximum amount of time a notification should be on screen for.
+ */
 const NotificationList: FunctionComponent<NotificationListProps> = (props) => {
   const items = props.notifications
     .slice(0, props.maxNotifications)
