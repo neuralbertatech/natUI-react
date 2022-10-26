d43d942cf0cb878636184d0ca94c8b41f05c2669
Giancarlo Pernudi Segura <gino@neuralberta.tech>
2022-10-25 23:14:52 -0400

    add box unit test

---
+++ tests/box.test.tsx
@@ -0,0 +1,30 @@
+import { Box } from "../src";
+import { render } from "@testing-library/react";
+
+describe("Box Component", () => {
+  test("box class property", () => {
+    const rendered = render(
+      <Box>Box 1</Box>
+    );
+
+    expect(rendered.getByText(/box/i)).toHaveClass("box");
+  });
+
+
+
+  test("no glass effect", () => {
+    const rendered = render(
+      <Box>Box 2</Box>
+    );
+
+    expect(rendered.getByText(/box/i)).not.toHaveClass("is-glass");
+  });
+
+  test("glass effect", () => {
+    const rendered = render(
+      <Box glass>Box 3</Box>
+    );
+
+    expect(rendered.getByText(/box/i)).toHaveClass("is-glass");
+  });
+});
