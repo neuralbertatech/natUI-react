18f45f7899f8a9927932d23e4c34eda89ae6f3ff
Yash Mouje <59363118+yashmouje@users.noreply.github.com>
2023-01-30 16:03:17 -0700

    Adding Radio Button Component

---
+++ src/components/RadioButton.tsx
@@ -0,0 +1,49 @@
+import BaseProps from "../types";
+import type { FunctionComponent } from "react";
+
+interface BaseRadioProps extends BaseProps {
+  text: string;
+  disabled?: boolean;
+  name: string;
+}
+
+
+export type RadioComponent = FunctionComponent<BaseRadioProps>
+/**
+ * Radio Button Component
+ * @component
+ * @param text {string} The text to display beside the button
+ * 
+ * @author Yash Sunil Mouje <yash_mouje@hotmail.com>
+ */
+const Radio: RadioComponent = (props) => {
+  const commonProps = {
+    text: props.text,
+    name: props.name,
+    disabled: props.disabled,
+  };
+
+  const innerDOM = (<>
+    {props.text}
+  </>);
+  return (props.disabled) ?
+    (
+      <label {...commonProps}>
+        <input type="radio" name={props.name} disabled />
+        {innerDOM}
+      </label>
+    ) :
+
+    (
+      <label {...commonProps}>
+        <input type="radio" name={props.name} />
+        {innerDOM}
+      </label>
+    );
+};
+
+Radio.defaultProps = {
+  disabled: false,
+};
+
+export default Radio;
