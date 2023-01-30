451ea8b3dc76650f717849ec118ee96f80c12152
Ganesh Saraswat <gsaraswa@ualberta.ca>
2023-01-30 17:20:37 +0000

    Added video component per the specifications. Also modified index.tsx, index.ts to show an example of it working.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import Notification from "./components/notification/Notification";
 import NotificationList from "./components/notification/NotificationList";
 import type { NotificationProps } from "./components/notification/Notification";
 import { NotificationStore } from "./utils";
+import Video from "./components/Video";
 
 export {
   Box,
@@ -25,5 +26,6 @@ export {
   NotificationProps,
   Card,
   CardBody,
-  CardButtons
+  CardButtons,
+  Video,
 };
