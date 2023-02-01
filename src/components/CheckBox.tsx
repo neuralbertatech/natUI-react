7f0f3fe79ea7358eadc8fecd053907bd6fdab7d6
Frank <ziang6@ualberta.ca>
2023-02-01 06:52:21 +0000

    starting implement checkbox

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,18 @@
+import type { FunctionComponent, ReactNode } from "react";
+
+import BaseProps from "../types";
+
+interface CheckBoxProps extends BaseProps{
+  children: ReactNode;
+}
+
+const CheckBox: FunctionComponent<CheckBoxProps> = (props) => {
+  return (
+    <label className={`checkbox`} style={props.style}>
+      <input type="checkbox" />
+      Remember me
+    </label>
+  );
+};
+
+export default CheckBox;
