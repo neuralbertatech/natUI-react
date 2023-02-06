d17e6e4d3052866e41447485b91be56707154149
Frank <ziang6@ualberta.ca>
2023-02-06 19:04:47 +0000

    add documentary, change style

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -2,20 +2,33 @@ import type { ChangeEvent, FunctionComponent } from "react";
 
 import BaseProps from "../types";
 
+/**
+ * @interface
+ * @property isChecked {boolean} wheather the box is checked 
+ * @property lable {string} the lable of the checkbox 
+ *
+ * @author Frank(Ziang) Li <ziang6@ualberta.ca> 
+ */
 interface CheckboxProps extends BaseProps{
   isChecked: boolean;
   handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
   lable: string;
 }
 
+/**
+ * CheckBox component
+ * @component
+ * @parm id {string} id of the checkbox input 
+ * @parm checked {boolean} if the checkbox is checked 
+ * @parm onChange {event} onChange function handles change event
+ * @author Frank(Ziang) Li <ziang6@ualberta.ca> 
+ */
 const Checkbox: FunctionComponent<CheckboxProps> = (props: CheckboxProps) => {
-
-
   return (
     <div style={{margin:"5px"}}>
       <label >
         {props.lable}
-        <input type="checkbox"  id={props.lable} checked={props.isChecked} onChange={props.handleChange} style={{marginLeft:"2px"}} />
+        <input type="checkbox"  id={props.lable} checked={props.isChecked} onChange={props.handleChange} style={{marginLeft:"5px"}} />
       </label>
     </div> 
 
