2b1a083988cc3d0e7f05ef480791d1c171942681
Yash Mouje <59363118+yashmouje@users.noreply.github.com>
2023-02-07 14:37:25 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { InputEmail, InputNumber, InputPassword, InputText } from "./components/input";
+import { InputEmail, InputNumber, InputPassword, InputText, Radio } from "./components/input";
 
 import Box from "./components/Box";
 import Button from "./components/Button";
@@ -10,7 +10,6 @@ import Notification from "./components/notification/Notification";
 import NotificationList from "./components/notification/NotificationList";
 import type { NotificationProps } from "./components/notification/Notification";
 import { NotificationStore } from "./utils";
-import Radio from "./components/input/Radio";
 
 export {
   Box,
