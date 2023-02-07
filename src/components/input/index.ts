b0984b830468839f3841a9fea41b2a8fed7da189
Yash Mouje <59363118+yashmouje@users.noreply.github.com>
2023-02-07 14:37:44 -0700

    Update index.ts

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -2,10 +2,12 @@ import InputEmail from "./InputEmail";
 import InputNumber from "./InputNumber";
 import InputPassword from "./InputPassword";
 import InputText from "./InputText";
+import Radio from "./Radio";
 
 export {
   InputEmail,
   InputNumber,
   InputPassword,
-  InputText
+  InputText,
+  Radio
 };
